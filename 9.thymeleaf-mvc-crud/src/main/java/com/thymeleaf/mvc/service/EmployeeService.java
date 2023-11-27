9ff709fd4fdadd11f5e20a28124089ddbe3a550c
PawelWodzynski <pawelwodzynski2001@gmail.com>
2023-11-27 20:51:47 +0100

    it definitely works

---
+++ 9.thymeleaf-mvc-crud/src/main/java/com/thymeleaf/mvc/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.thymeleaf.mvc.service;
+
+import com.thymeleaf.mvc.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
