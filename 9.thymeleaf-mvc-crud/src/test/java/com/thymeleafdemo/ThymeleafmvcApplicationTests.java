8684a78d493175c8133256538c3578726ebac8dd
Pawel Wodzynski <pawelwodzynski2001@gmail.com>
2023-12-04 19:03:25 +0100

    Completed MVC-CRUD Project - Merged 'MVC-CRUD' into 'Master' to finalize the project - Deleted 'MVC-CRUD' branch after successful integration

---
+++ 9.thymeleaf-mvc-crud/src/test/java/com/thymeleafdemo/ThymeleafmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
