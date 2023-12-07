080597d24d74c0741968c638c16a52d1ca208b61
Pawel Wodzynski <pawelwodzynski2001@gmail.com>
2023-12-07 20:14:38 +0100

    Authorization v3
    - Updated DemoSecurityConfig class, configured a custom page for access denied.
    - Updated LoginController class, added a new request for '/access-denied'.
    - Created 'access-denied.html' for non-authorized users.
    
    This commit enhances the user experience by customizing the access denied page and providing a dedicated HTML template for users without proper authorization.

--- 10.thymeleaf-mvc-security/src/main/java/com/mvc/security/controller/LoginController.java
+++ 10.thymeleaf-mvc-security/src/main/java/com/mvc/security/controller/LoginController.java
@@ -5,13 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-
+// (Login controller = security controller)
     @GetMapping("/showMyLoginPage")
     public String showMyLoginPage(){
 
 //        return "plain-login";
         return "fancy-login";
     }
+
+    // add request mapping for /acces-denied
+    @GetMapping("/access-denied")
+    public String showAccesDenied(){
+        return "access-denied";
+    }
+
 
 
 }
