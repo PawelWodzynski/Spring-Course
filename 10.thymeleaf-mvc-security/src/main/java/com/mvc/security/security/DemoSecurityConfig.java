c2cd3b2552fd0dd914871cb85d763fcd20c8ad73
Pawel Wodzynski <pawelwodzynski2001@gmail.com>
2023-12-09 22:25:37 +0100

    JDBC Authentication Configuration
    - Updated DemoSecurityConfig class to configure JDBC authentication.

--- 10.thymeleaf-mvc-security/src/main/java/com/mvc/security/security/DemoSecurityConfig.java
+++ 10.thymeleaf-mvc-security/src/main/java/com/mvc/security/security/DemoSecurityConfig.java
@@ -3,37 +3,21 @@ package com.mvc.security.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
 
+    // add suport for JDBC ... no more hardcoded users c:
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}shelby")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails arthur = User.builder()
-                .username("arthur")
-                .password("{noop}shelby")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
-
-        UserDetails thomas = User.builder()
-                .username("thomas")
-                .password("{noop}shelby")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
-                .build();
-
-        return  new InMemoryUserDetailsManager(john,arthur,thomas);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
 
@@ -61,7 +45,29 @@ public class DemoSecurityConfig {
     }
 
 
-
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}shelby")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails arthur = User.builder()
+//                .username("arthur")
+//                .password("{noop}shelby")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//
+//        UserDetails thomas = User.builder()
+//                .username("thomas")
+//                .password("{noop}shelby")
+//                .roles("EMPLOYEE","MANAGER","ADMIN")
+//                .build();
+//
+//        return  new InMemoryUserDetailsManager(john,arthur,thomas);
+//    }
 
 
 }
