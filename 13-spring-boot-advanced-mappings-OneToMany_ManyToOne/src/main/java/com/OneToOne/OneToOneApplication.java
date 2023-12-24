887fa78c7cbae444df374851d3415fa2ef1ce0f5
Pawel Wodzynski <pawelwodzynski2001@gmail.com>
2023-12-24 16:24:37 +0100

    @OneToMany Lazy find courses
     - Updated Instructor entity class, changed fetch type to lazy, only for readability
    
     - Updated AppDAO interface, added method for find courses by instructor id
    
     - Updated AppDAOImpl class, implemented method from AppDAO interface and introduce query and logic for find courses by instructor id
    
     - Updated main app, added new method for find instructor courses with fetch type LAZY

--- 13-spring-boot-advanced-mappings-OneToMany_ManyToOne/src/main/java/com/OneToOne/OneToOneApplication.java
+++ 13-spring-boot-advanced-mappings-OneToMany_ManyToOne/src/main/java/com/OneToOne/OneToOneApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class OneToOneApplication {
 
@@ -31,8 +33,31 @@ public class OneToOneApplication {
 
 //			createInstructorWithCourses(appDAO);
 
-			findInstructorWithCourse(appDAO);
+//			findInstructorWithCourse(appDAO);
+
+			findCoursesForInstructor(appDAO); // find instructor courses by id witch LAZY fetch type
 		};
+	}
+
+	private void findCoursesForInstructor(AppDAO appDAO) {
+		int theId = 1;
+		//find instructor
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor" + tempInstructor);
+
+		// find course for instructor
+		System.out.println("Finding courses for instructor id: " + theId);
+		List<Course> courses = appDAO.findCoursesByInstructorId(theId);
+
+		//associate the objects
+		tempInstructor.setCourses(courses);
+
+		System.out.println("the associated courses: " + tempInstructor.getCourses());
+
+		System.out.println("DONE");
 	}
 
 	private void findInstructorWithCourse(AppDAO appDAO) {
--- 13-spring-boot-advanced-mappings-OneToMany_ManyToOne/src/main/java/com/OneToOne/dao/AppDAO.java
+++ 13-spring-boot-advanced-mappings-OneToMany_ManyToOne/src/main/java/com/OneToOne/dao/AppDAO.java
@@ -1,19 +1,22 @@
 package com.OneToOne.dao;
 
+import com.OneToOne.entity.Course;
 import com.OneToOne.entity.Instructor;
 import com.OneToOne.entity.InstructorDetail;
+
+import java.util.List;
 
 public interface AppDAO {
 
     void save(Instructor theInstructor);
 
-
     Instructor findInstructorById(int theId);
-
 
     void deleteInstructorById(int theId);
 
     InstructorDetail findInstructorDetailById(int theId);
 
     void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
 }
