83da3b37516f7e094f8bcb12d530d03d04a93bc1
Pawel Wodzynski <pawelwodzynski2001@gmail.com>
2023-12-24 15:56:42 +0100

    @OneToMany fetch types - EAGER vs LAZY
     - Updated main app, added new method for find instructor with course
    
     - Updated Instructor entity class, added fetch type EAGER for Course List field
    
     if you want test LAZY fetch type, stay without defined fech type, because default fetch type for @OneToMany annotation is LAZY...

--- 13-spring-boot-advanced-mappings-OneToMany_ManyToOne/src/main/java/com/OneToOne/OneToOneApplication.java
+++ 13-spring-boot-advanced-mappings-OneToMany_ManyToOne/src/main/java/com/OneToOne/OneToOneApplication.java
@@ -29,9 +29,22 @@ public class OneToOneApplication {
 
 //			deleteInstructorDetail(appDAO);
 
-			createInstructorWithCourses(appDAO);
+//			createInstructorWithCourses(appDAO);
 
+			findInstructorWithCourse(appDAO);
 		};
+	}
+
+	private void findInstructorWithCourse(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor" + tempInstructor);
+		System.out.println("the associated courses " + tempInstructor.getCourses());
+
+		System.out.println("DONE");
 	}
 
 	private void createInstructorWithCourses(AppDAO appDAO) {
